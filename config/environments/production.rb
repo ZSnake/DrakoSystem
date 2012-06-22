9361a847038b6c74ea3b73a85588b685f5a49697
Solid Snake <viktor_zavala@unitec.edu>
2012-06-22 05:23:38 -0600

    third try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,10 @@ DrakoSystem::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching = false
+  config.action_view.debug_rjs
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
