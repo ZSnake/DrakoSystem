c5e90d5e5ee7e49a490c88815118ef6b65faa17c
Solid Snake <viktor_zavala@unitec.edu>
2012-06-22 04:58:38 -0600

    second try heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ DrakoSystem::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -16,10 +16,9 @@ DrakoSystem::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.assets.initialize_on_precompile = false
   # Generate digests for assets URLs
   config.assets.digest = true
-
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
